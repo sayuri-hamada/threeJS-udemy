a54d2a9e36ac2ede47f1926e6360bc0bacf19f56
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2023-06-22 15:40:06 +0900

    ポイント光源の場所を特定してみよう

--- main.js
+++ main.js
@@ -44,5 +44,9 @@ pointLight = new THREE.PointLight(0xffffff, 1);
 pointLight.position.set(-200, -200, -200);
 scene.add(pointLight);
 
+// ポイント光源がどこにあるのかを特定する
+let pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
+scene.add(pointLightHelper);
+
 // レンダリングしてみよう
 renderer.render(scene, camera);
