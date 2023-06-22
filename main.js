4578375082ee369639ff9c4337ae5ed0122af32e
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2023-06-22 15:02:01 +0900

    カメラを作成してみよう

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ import './style.css'
 
 import * as THREE from 'three';
 
-let scene;
+let scene, camera;
 
 // シーンを追加
 scene = new THREE.Scene();
 
+// カメラを追加
+camera = new THREE.PerspectiveCamera(
+  50, //視野角
+  window.innerWidth / window.innerHeight, //アスペクト比
+  0.1, //開始距離
+  1000 //終了距離
+);
