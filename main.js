8df3e800b974410fea7e05ddcde91acf63703dee
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2023-06-22 15:07:09 +0900

    レンダラーを作成してみよう

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 import * as THREE from 'three';
 
-let scene, camera;
+let scene, camera, renderer;
 
 // シーンを追加
 scene = new THREE.Scene();
@@ -14,3 +14,12 @@ camera = new THREE.PerspectiveCamera(
   0.1, //開始距離
   1000 //終了距離
 );
+
+// レンダラーを追加
+renderer = new THREE.WebGLRenderer();
+// レンダラーのサイズを調整
+renderer.setSize(window.innerWidth, window.innerHeight);
+// DOM追加
+document.body.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
