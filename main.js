eecdcdecf2b970deb3614dc2eb71fb5d8ad05bfd
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2023-06-23 12:43:42 +0900

    init

--- main.js
+++ main.js
@@ -1,103 +1,72 @@
-import './style.css'
-
+import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let scene, camera, renderer, pointLight, controls;
+//シーン
+const scene = new THREE.Scene();
 
-window.addEventListener('load', init);
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.set(1, 1, 2);
 
+//レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
 
-function init() {
-  // シーンを追加
-  scene = new THREE.Scene();
+/**
+ * ジオメトリを作ってみよう。
+ **/
 
-  // カメラを追加
-  camera = new THREE.PerspectiveCamera(
-    50, //視野角
-    window.innerWidth / window.innerHeight, //アスペクト比
-    0.1, //開始距離
-    1000 //終了距離
-  );
-  camera.position.set(0, 0, 500);
+//マテリアル
+const material = new THREE.MeshNormalMaterial();
 
-  // レンダラーを追加
-  renderer = new THREE.WebGLRenderer({alpha: true});
-  // レンダラーのサイズを調整
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  // 解像度を調整
-  renderer.setPixelRatio(window.devicePixelRatio);
-  // DOM追加
-  document.body.appendChild(renderer.domElement);
+//ライト
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
 
-  // テクスチャーを追加してみよう
-  let textures = new THREE.TextureLoader().load('/textures/earth.jpg');
+//マウス操作
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
-  // ジオメトリを作成
-  let ballGeometry = new THREE.SphereGeometry(100, 64, 32) // 半径、ポリゴンの数（大きくするとより球体に近づく）
+window.addEventListener("resize", onWindowResize);
 
-  // マテリアルを作成
-  let ballMaterial = new THREE.MeshPhysicalMaterial({map: textures});
-
-  // メッシュ化してみよう
-  let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-
-  scene.add(ballMesh);
-
-  // 平行光源を追加してみよう
-  let directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-  directionalLight.position.set(1, 1, 1);
-  scene.add(directionalLight);
-
-  // ポイント光源を追加してみよう
-  pointLight = new THREE.PointLight(0xffffff, 1);
-  pointLight.position.set(-200, -200, -200);
-  scene.add(pointLight);
-
-  // ポイント光源がどこにあるのかを特定する
-  let pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
-  scene.add(pointLightHelper);
-
-  // マウス操作ができるようにする
-  controls = new OrbitControls(camera, renderer.domElement);
-
-  window.addEventListener('resize', onWindowRisize);
-
-  animate();
-}
-
-// ブラウザのリサイズに対応させよう
-function onWindowRisize() {
-  // レンダラーのサイズを随時更新
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  // カメラのアスペクト比を正す
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-}
-
-
-// let rot = 0;
+const clock = new THREE.Clock();
 
 function animate() {
-  // ポイント光源を球の周りを巡回させよう
-  pointLight.position.set(
-    200 * Math.sin(Date.now() / 500),
-    200 * Math.sin(Date.now() / 1000),
-    200 * Math.cos(Date.now() / 500),
-  );
+  const elapsedTime = clock.getElapsedTime();
+  // console.log(elapsedTime);
 
-  // カメラを回転させてみよう
-  // rot += 0.5;
-  // let radian = rot * (Math.PI / 180); //ラジアンに変換
-  // camera.position.x = Math.sin(radian) * 500;
-  // camera.position.z = Math.cos(radian) * 500;
-  // camera.lookAt(ballMesh.position);
+  //オブジェクトの回転
+  // sphere.rotation.x = elapsedTime;
+  // plane.rotation.x = elapsedTime;
+  // octahedron.rotation.x = elapsedTime;
+  // torus.rotation.x = elapsedTime;
 
-  // レンダリングしてみよう
+  // sphere.rotation.y = elapsedTime;
+  // plane.rotation.y = elapsedTime;
+  // octahedron.rotation.y = elapsedTime;
+
+  // torus.rotation.y = elapsedTime;
+
+  controls.update();
+
+  //レンダリング
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
 
+//ブラウザのリサイズに対応
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
 
-
-
+animate();
